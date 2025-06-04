da0c59f6b3868dbffebd3c6a7d1a1e2fdcc63d76
CLee020 <Carter.lee26@ycdsbk12.ca>
2025-06-04 09:43:15 -0400

    Worked on Menu and help menu

--- CPTcarter1.java
+++ CPTcarter1.java
@@ -2,79 +2,125 @@ import arc.*;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.Font;
+import java.util.Arrays;
 
 public class CPTcarter1{
 	public static void main(String[] args){
 		Console con = new Console("BlackJack", 1280, 720);
+		// Variables
+		Font fntTNRjoke = con.loadFont("times.ttf", 10);
+		Font fntTNR = con.loadFont("times.ttf", 35);
+		Font fntTNRsmall = con.loadFont("times.ttf", 20);
+		BufferedImage imgPoker = con.loadImage("Blackjack.png");
+		
+		
 		// Main Menu
 		// Background
 		con.setBackgroundColor(new Color(255, 255, 255));
 		
-		// Words
+		// Menu Drawings / Words
 		con.setDrawColor(new Color(211, 211, 211));
-		Font fntTNRjoke = con.loadFont("times.ttf", 10);
 		con.setDrawFont(fntTNRjoke);
 		con.drawString("super secret joke :) [ J ]", 0, 0);
 		
 		con.setDrawColor(new Color(0, 0, 0));
-		Font fntTNR = con.loadFont("times.ttf", 35);
 		con.setDrawFont(fntTNR);
 	
 		con.drawString("Main Menu", 560, 300);
 		con.drawString("Play [ P ]", 570, 340);
 		con.drawString("Leaderboard [ L ]", 530, 380);
 		con.drawString("Help [ H ]", 570, 420);
-		con.drawString("Quit [ Q ]", 570, 460);
+		con.drawString("Exit [ X ]", 570, 460);
 		
-		
-		BufferedImage imgPoker = con.loadImage("Blackjack.png");
 		con.drawImage(imgPoker, 512, 30);
 		con.repaint();	
 			
 		// Inputs
 		char chrInput = con.getChar();
+		System.out.println("entered letter: "+chrInput);
 		
-		if(chrInput == 'P' || chrInput == 'p'){ 
+		if(chrInput == 'P' || chrInput == 'p'){ // Playing game
 			con.clear();
 			con.setBackgroundColor(new Color(0, 0, 0));
 			
-		}else if(chrInput == 'L' || chrInput == 'l'){
+		}else if(chrInput == 'L' || chrInput == 'l'){ // Leaderboard
 			con.clear();
 			
+		}else if(chrInput == 'H' || chrInput == 'h'){ // Help
+			con.clear();
+			con.setBackgroundColor(new Color(255, 255, 255));
+			
+			con.drawImage(imgPoker, 512, 30);
+			
+			con.setDrawColor(new Color(0, 0, 0));
+			con.setDrawFont(fntTNR);
+			
+			con.drawString("Help Menu", 560, 300);
+			con.drawString("Basic Rules [ R ]", 560, 300);
+			con.drawString("Special Rules [ S ]", 560, 340);
+			con.drawString("Credits [ C ]", 550, 380);
+			con.repaint();	
+			
+			char chrHelpInput = con.getChar();
+			System.out.println("entered letter: "+chrHelpInput);
+			
+			if(chrHelpInput == 'R' || chrHelpInput == 'r'){ // Basic Rules
+				con.clear();
+				con.setBackgroundColor(new Color(255, 255, 255));
+				con.setDrawColor(new Color(0, 0, 0));
+				con.setDrawFont(fntTNRsmall);
+				
+				
+				
+			}else if(chrHelpInput == 'S' || chrHelpInput == 's'){ // Special Rules
+				con.clear();
+				con.setBackgroundColor(new Color(255, 255, 255));
+				con.setDrawColor(new Color(0, 0, 0));
+				con.setDrawFont(fntTNRsmall);
+				con.drawString("Here are some of the special features that make the game called BlackJack", 300, 300);
+				con.drawString("Double Down - If your first 2 cards add up to 9, 10 or 11, you will double your bet and draw one more card.", 200, 340);
+				con.drawString("Five Cards - If you reach 5 cards, you automatically get 3x your bet but same applies to dealers.", 230, 380);
+				con.drawString("Black Jack - If your first two cards equals to 21, you multiply 3x your original bet", 290, 420);
+				con.drawString("Exit [ X ]",570, 460);
+				con.repaint();
+				
+				char chrHelpExit = con.getChar();
+				System.out.println("entered letter: "+chrHelpExit);
+				
+				if(chrHelpExit == 'X' || chrHelpExit == 'x'){
+					con.closeConsole();
+				}
+			}else if(chrHelpInput == 'C' || chrHelpInput == 'c'){ // Credits
+				con.clear();
+				con.setBackgroundColor(new Color(255, 255, 255));
+				con.setDrawColor(new Color(0, 0, 0));
+				con.setDrawFont(fntTNR);
+				con.drawString("Made By Carter :)", 520, 300);
+				con.drawString("Exit [ X ]",570, 340);
+				con.repaint();
+				
+				char chrHelpExit = con.getChar();
+				System.out.println("entered letter: "+chrHelpExit);
+				if(chrHelpExit == 'X' || chrHelpExit == 'x'){
+					con.closeConsole();
+				}
+				
+			}
+		}else if(chrInput == 'X' || chrInput == 'x'){ // Exit
+			con.closeConsole();
+		}else if(chrInput == 'J' || chrInput == 'j'){ // Joke
+			con.clear();
+			con.setBackgroundColor(new Color(255, 255, 255));
+			con.setDrawColor(new Color(0, 0, 0));
+			con.setDrawFont(fntTNR);
+			con.drawString("Why wasn't the Cactus invited to hangout with the Mushrooms?", 200, 300);
+			con.drawString("He wasn't a FUNGI!", 520, 340);
+			con.repaint();
 		}
 			
-			
-			
-		// Card array
+		// Gameplay
 		
-		int[][] intCards = new int[14][5];
 		
-		intCards[1][1] = 1;
-		intCards[2][1] = 2;
-		intCards[3][1] = 3;
-		intCards[4][1] = 4;
-		intCards[5][1] = 5;
-		intCards[6][1] = 6;
-		intCards[7][1] = 7;
-		intCards[8][1] = 8;
-		intCards[9][1] = 9;
-		intCards[10][1] = 10;
-		intCards[11][1] = 11;
-		intCards[12][1] = 12;
-		intCards[13][1] = 13;
-		intCards[1][2] = 14;
-		intCards[2][2] = 15;
-		intCards[3][2] = 16;
-		intCards[4][2] = 17;
-		intCards[5][2] = 18;
-		intCards[6][2] = 19;
-		intCards[7][2] = 20;
-		intCards[8][2] = 21;
-		intCards[9][2] = 22;
-		intCards[10][2] = 23;
-		intCards[11][2] = 24;
-		intCards[12][2] = 25;
-		intCards[13][2] = 26;
 		
 	
 		
--- Cartertools.java
+++ Cartertools.java
@@ -4,6 +4,54 @@ public class Cartertools{
 
 	public static class Buttons{
 		
+		public static int[][] deckOfCards(){
 		
+		int intDeck[][] = new int[52][3];
+		int intCards = 0;
+		int intValue = 0;
+		int intSuit = 0;
+		
+		for(intSuit = 0; intSuit <= 3; intSuit++){
+			for(intValue = 0; intValue <= 12; intValue++){
+				intDeck[intCards][0] = intValue;
+				intDeck[intCards][1] = intSuit;
+				intDeck[intCards][2] = (int)(Math.random() * 100 + 1);
+				intCards++;
+			}
+		}
+		
+		int intVTemp;
+		int intSTemp;
+		int intRITemp;
+		
+		// Sorting
+		for(int intCount2 = 0; intCount2 < intCards - 1; intCount2++){ 
+			for(int intCount = 0; intCount < intCards - 1; intCount++){
+				if(intDeck[intCount][2] > intDeck[intCount+1][2]){
+					
+					// Swapping card value
+					intVTemp = intDeck[intCount][0];
+					intDeck[intCount][0] = intDeck[intCount+1][0];
+					intDeck[intCount+1][0] = intVTemp;
+					
+					// Swapping suit value
+					intSTemp = intDeck[intCount][1];
+					intDeck[intCount][1] = intDeck[intCount+1][1];
+					intDeck[intCount+1][1] = intSTemp;
+					
+					// Swapping random integer value 
+					intRITemp = intDeck[intCount][2];
+					intDeck[intCount][2] = intDeck[intCount+1][2];
+					intDeck[intCount+1][2] = intRITemp;
+					}
+				}
+			}
+		
+		for(int intCount = 0; intCount < intCards; intCount++){
+			System.out.println(intDeck[intCount][0] + " - " + intDeck[intCount][1] + " - " + intDeck[intCount][2]);
+			}
+		
+		return intDeck;
+		}
 	}
 }
